13de32f9a1a06834dce2bd2c5ef7c3c1cf67f420
Ceesay Fofana <22731598+cfofana@users.noreply.github.com>
2023-08-29 00:58:49 -0400

    Use common html tags in app.js and style them

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>This is a very simple page</h1>
       </header>
+      <main>
+        <h2>This is a heading</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui incidunt, veritatis repellendus nulla necessitatibus nisi molestias voluptatum quasi soluta voluptatem consequuntur consequatur magnam inventore aperiam ullam. Explicabo ad quas repudiandae.</p>
+      </main>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
