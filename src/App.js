091c10971b2412843e3c2bc73f71d3a9da59b1ab
Ceesay Fofana <22731598+cfofana@users.noreply.github.com>
2023-08-30 00:57:41 -0400

    Add button and form components and use them and style them

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import './App.css';
 
+
+function MyButton(){
+  return (
+    <button className="btn">This is a button</button>
+  );
+}
+
+function MyForm(){
+  return(
+    <form>
+      <label for="name">Name</label>
+      <input type="text" id="name" name="name" /><br/>
+      <label for="email">Email</label>
+      <input type="email" id="email" name="email" /><br/>
+      <label for="message">Message</label>
+      <textarea id="message" name="message" rows="10" cols="30"></textarea>
+    </form>
+  );
+}
+
 function App() {
   return (
     <div className="container">
@@ -10,6 +30,8 @@ function App() {
         <h2>This is a heading</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui incidunt, veritatis repellendus nulla necessitatibus nisi molestias voluptatum quasi soluta voluptatem consequuntur consequatur magnam inventore aperiam ullam. Explicabo ad quas repudiandae.</p>
       </main>
+      <MyButton />
+      <MyForm />
     </div>
     // <div className="App">
     //   <header className="App-header">
